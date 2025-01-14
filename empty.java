f0f60312cd14eee0031a1254fed7b0142c822421
dsterckx <39022656+daansterckx@users.noreply.github.com>
2025-01-14 13:57:15 +0100

    Update empty.java

--- empty.java
+++ empty.java
@@ -1,7 +1,7 @@
-class Test
-{
-    public static void main(String[] args)
-    {
-        System.out.println("My First Java Program.");
+package com.example;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
     }
 }
